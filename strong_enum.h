10d504363ed02b6c36e21fc7369b5ca437d9646f
karan bopche <34151981+karanbopche@users.noreply.github.com>
2026-04-08 14:10:32 +0530

    Modify STRONG_ENUM_DECLARE to accept int_type
    allow selecting underlaying int value for storing values of enum

--- strong_enum.h
+++ strong_enum.h
@@ -1,7 +1,7 @@
 #ifndef STRONG_ENUM_H
 #define STRONG_ENUM_H
 
-#define STRONG_ENUM_DECLARE(enum_type_name) typedef struct {int val;} enum_type_name;
+#define STRONG_ENUM_DECLARE(enum_type_name, int_type) typedef struct {int_type val;} enum_type_name;
 #define STRONG_ENUM_DECLARE_VALUE(enum_value_name, value)  (enum_value_name){value}
 #define STRONG_ENUM_IS_EQUAL(a, b) ((a).val == (b).val)
 #define STRONG_ENUM_COMPARE(a, b) ((a).val - (b).val)
