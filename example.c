f6fae7c58ee69542f4cbca4fa8d7a1d70f55828f
karan bopche <34151981+karanbopche@users.noreply.github.com>
2026-04-08 14:11:17 +0530

    Refactor strong enum declarations for MyEnum1 and MyEnum2

--- example.c
+++ example.c
@@ -5,18 +5,19 @@
  */
 #include "strong_enum.h"
 
-
-STRONG_ENUM_DECLARE(MyEnum1)
+STRONG_ENUM_DECLARE(MyEnum1, int)
 #define MyEnum1_Value1   STRONG_ENUM_DECLARE_VALUE(MyEnum1, 1);
 #define MyEnum1_Value2   STRONG_ENUM_DECLARE_VALUE(MyEnum1, 2);
 
-STRONG_ENUM_DECLARE(MyEnum2)
+
+STRONG_ENUM_DECLARE(MyEnum2, int)
 #define MyEnum2_Value1   STRONG_ENUM_DECLARE_VALUE(MyEnum2, 10);
 #define MyEnum2_Value2   STRONG_ENUM_DECLARE_VALUE(MyEnum2, 20);
 
 // defining strong enums variables
 MyEnum1 enumVar1 = MyEnum1_Value1;
 MyEnum2 enumVar2 = MyEnum2_Value1;
+
 
 // example functions accepting strong enums as parameters
 void function1(MyEnum1 e) {}
